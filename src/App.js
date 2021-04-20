1bde674394b1f514036d8af52d060e9e2c669631
Hugo Fitas <hfitas@gmail.com>
2021-04-20 20:27:32 +0100

    finished router challenge

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from "react";
+import { Route, Switch, Redirect } from "react-router";
 import MovieList from "./components/MovieList";
+import MovieDetails from "./components/MovieDetails";
 
 function App() {
   return (
     <div className="App">
       <MovieList />
+      <Switch>
+        <Route exact path="/movies" component={MovieList} />
+        <Route path="/movies/:id" component={MovieDetails} />
+        <Redirect to="/movies" />
+      </Switch>
     </div>
   );
 }
